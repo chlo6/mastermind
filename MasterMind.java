86c08279741236a4d080b75b894d2b878319eb2f
chloe <162272068+chlo6@users.noreply.github.com>
2024-10-29 22:19:28 -0700

    Update MasterMind.java

--- MasterMind.java
+++ MasterMind.java
@@ -1,9 +1,26 @@
 /**
  *	Plays the game of MasterMind.
- *	<Describe the game here>
- *	@author
- *	@since
+ * 	Objective: Guess the unknown 4-letter code, consisting of 6 
+ * 	possible letters (A, B, C, D, E, F) in 10 turns or less. The letters 
+ * 	can repeat, and some examples of codes are ABCD, FDDE, and BBBB.  
+ * 
+ * 	For each turn, you are prompted to guess, then a board will be 
+ * 	displayed. This board contains your previous and current guesses, 
+ * 	along with how many letters of each guess were partially correct 
+ * 	(same letter, different position) or completely correct (same 
+ * 	letter, same position). For example, if the 4-letter code was ABDC, 
+ * 	and your guess was DBBF, then you would have 1 partially correct 
+ * 	letter (D) and 1 fully correct letter (the first B). 
+ * 
+ * 	To run, cd into the directory with MasterMind.java, Dice.java, 
+ * 	Prompt.java, Peg.java, and PegArray.java, then type in your terminal 
+ * 	"javac -source 1.8 -target 1.8 MasterMind.java; java MasterMind" and 
+ * 	it should run
+ *
+ *	@author	Chloe He
+ *	@since	September 27, 2024
  */
+
 
 public class MasterMind {
 
@@ -11,25 +28,64 @@ public class MasterMind {
 	private PegArray[] guesses;		// the array of guessed peg arrays
 	private PegArray master;		// the master (key) peg array
 	
+	private String inputStr = "";		// stores current guess
+	private boolean gameLost = false;	// if the game was lost
+	private boolean gameWon = false;	// if the game was won
+	private int turn = 1;				// the # turn you are currently on
+
 	// Constants
 	private final int PEGS_IN_CODE = 4;		// Number of pegs in code
 	private final int MAX_GUESSES = 10;		// Max number of guesses
 	private final int PEG_LETTERS = 6;		// Number of different letters on pegs
 											// 6 = A through F
 
+	/**
+	 * 	Constructor
+	 * 	Initializes field variables, e.g. master PegArray and guesses
+	 * 	PegArray
+	 */
 	public MasterMind() {
-		master = new PegArray(4);
+		master = new PegArray(PEGS_IN_CODE);
+		guesses = new PegArray[MAX_GUESSES];
+		for (int i = 0; i < MAX_GUESSES; i++) guesses[i] = new PegArray(PEGS_IN_CODE);
 	}
 	
+	/** 
+	 * 	Main class
+	 * 	Calls the method that runs the game
+	 */
 	public static void main (String [] args) {
 		MasterMind run = new MasterMind();
-		run.runner();
+		run.playGame();
 	}
 	
-	public void runner() {
+	/** 
+	 * 	Method that runs most of the game
+	 * 	Calls other methods (e.g. for playing each turn and getting
+	 * 	the user input) in order, checks if the game has been completed
+	 */
+	public void playGame() {
 		printIntroduction();
-		
+		Prompt.getString("Hit the Enter key to start the game");
+
+		printBoard();
 		genMasterCode();
+		
+		
+		while (!gameWon && !gameLost) {
+			System.out.println("\nGuess " + turn + "\n");
+			userInput();
+			playTurn();
+			printBoard();
+			
+			turn++;			
+			if (turn > MAX_GUESSES) gameLost = true; 
+		}
+		
+		if (gameWon) {
+			turn--;
+			System.out.println("\nNice work! You found the master code in " + turn + " guesses.");
+		} else System.out.println("Oops. You were unable to find the solution in 10 guesses.");
 	}
 
 	/**
@@ -62,17 +118,22 @@ public class MasterMind {
 		System.out.println("+------------------------------------------------------------------------------------+");
 		System.out.println("\n");
 	}
+
 	
-	
+	/**
+	 * 	Method that generates the master code, uses dice to generate
+	 * 	a number between 1-6 (representing a-f), then converts it to a 
+	 * 	char
+	 */ 
 	public void genMasterCode() {
 		Dice dice = new Dice();
 		for (int i = 0; i < 4; i++) {
-			Peg peg = new Peg((char)(dice.roll() + 65));
+			Peg peg = new Peg((char)(dice.roll() + 64));
 			master.setPeg(i, peg);
 			
 		}
-		master.printPegs();
 	}
+	
 	/**
 	 *	Print the peg board to the screen
 	 */
@@ -114,15 +175,55 @@ public class MasterMind {
 		System.out.printf("|   %2d   |", (t + 1));
 		// If peg letter in the A to F range
 		char c = guesses[t].getPeg(0).getLetter();
-		if (c >= 'A' && c <= 'F')
-			for (int p = 0; p < PEGS_IN_CODE; p++)
+		if (c >= 'A' && c <= 'F') {
+			for (int p = 0; p < PEGS_IN_CODE; p++){
 				System.out.print("   " + guesses[t].getPeg(p).getLetter() + "   |");
+			}
+			guesses[t].getExactMatches(master);
+			guesses[t].getPartialMatches(master);
+		}
 		// If peg letters are not A to F range
-		else
-			for (int p = 0; p < PEGS_IN_CODE; p++)
+		else {
+			for (int p = 0; p < PEGS_IN_CODE; p++) {
 				System.out.print("       |");
-		System.out.printf("   %d      %d    |\n",
-							guesses[t].getExact(), guesses[t].getPartial());
+			}
+		}
+		
+		System.out.printf("   %d      %d    |\n", guesses[t].getExact(), guesses[t].getPartial());
+		if (guesses[t].getExact() == 4) {
+			gameWon = true;
+		}
 	}
-
+	
+	/**
+	 * 	Gets user's input, checks its validity
+	 */
+	public void userInput() {
+		boolean found = false;
+		while (!found) {
+			found = true;
+			inputStr = Prompt.getString("Enter the code using (A,B,C,D,E,F). For example, ABCD or abcd from left-to-right");
+			if (inputStr.length() != PEGS_IN_CODE) found = false;
+			else {
+				for (int i = 0; i < PEGS_IN_CODE; i++) {
+					char ch = Character.toLowerCase(inputStr.charAt(i));
+					if (ch < 'a' || ch > 'f') found = false;
+				}
+			} 
+			inputStr = inputStr.toUpperCase();
+			
+			if (!found) System.out.println("ERROR: Bad input, try again.");
+		}
+	}
+	
+	/**
+	 * 	Plays each turn by storing the guessed pegs in the guesses 
+	 * 	PegArray
+	 */
+	public void playTurn() {
+		for (int i= 0; i < PEGS_IN_CODE; i++) {
+			Peg guessPeg = new Peg(inputStr.charAt(i));
+			guesses[turn-1].setPeg(i, guessPeg);
+		}
+	}
 }
